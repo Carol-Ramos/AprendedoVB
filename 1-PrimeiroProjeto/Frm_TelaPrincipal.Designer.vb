a979c7b6ba8704825e84b9b317df19c1724b4fbf
Carol <carolineramos98@outlook.com>
2022-04-09 21:03:56 -0300

    Aula2

--- 1-PrimeiroProjeto/Frm_TelaPrincipal.Designer.vb
+++ 1-PrimeiroProjeto/Frm_TelaPrincipal.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class Frm_TelaPrincipal
     Inherits System.Windows.Forms.Form
 
     'Descartar substituições de formulário para limpar a lista de componentes.
@@ -22,29 +22,29 @@ Partial Class Form1
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Btm_Principal = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Button1
+        'Btm_Principal
         '
-        Me.Button1.Location = New System.Drawing.Point(285, 163)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(175, 88)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Btm_Principal.Location = New System.Drawing.Point(285, 163)
+        Me.Btm_Principal.Name = "Btm_Principal"
+        Me.Btm_Principal.Size = New System.Drawing.Size(175, 88)
+        Me.Btm_Principal.TabIndex = 0
+        Me.Btm_Principal.Text = "Clique Aqui"
+        Me.Btm_Principal.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Btm_Principal)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Aplicação Alô Mundo"
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents Btm_Principal As Button
 End Class
---
+++ 1-PrimeiroProjeto/Frm_TelaPrincipal.vb
@@ -0,0 +1,7 @@
+﻿Public Class Frm_TelaPrincipal
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Btm_Principal.Click
+
+        MsgBox("Olá Mundo!")
+
+    End Sub
+End Class
